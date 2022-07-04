4df57a05cf9f92e6478be18e2c9ccc8b785e6f13
Jonathan Rodriguez <ing.rodriguezja@gmail.com>
2022-07-04 18:44:51 +0200

    swagger implementation

--- src/main/java/com/test/sanitas/SanitasApplication.java
+++ src/main/java/com/test/sanitas/SanitasApplication.java
@@ -2,12 +2,21 @@ package com.test.sanitas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class SanitasApplication {
+public class SanitasApplication implements WebMvcConfigurer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SanitasApplication.class, args);
 	}
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	    registry.addResourceHandler("swagger-ui.html")
+	      .addResourceLocations("classpath:/META-INF/resources/");
 
+	    registry.addResourceHandler("/webjars/**")
+	      .addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
 }
